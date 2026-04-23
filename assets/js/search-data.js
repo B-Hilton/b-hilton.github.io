5554ac2fe2f65b6f4e93a4a1d8b8179921a3c3df
B-Hilton <bryanghilton@gmail.com>
2026-04-23 02:14:44 +0000

    Deploying to gh-pages from @ B-Hilton/b-hilton.github.io@09c0b77c7e0f640ff21715cd18721202a0850d41 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -458,7 +458,7 @@ ninja.data = [{
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          description: "GOKarter Ewell",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
@@ -466,9 +466,9 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-hv-research",
+          title: 'HV research',
+          description: "for surface wakefield proton acceleration",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
